a97d4119a375a175b0df93adb1b37d3171cfdbc9
Alvaro <alvaro.touzon@bbva.com>
2025-02-10 13:39:42 +0100

    feat: add apply .ap to task

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const { Task } = require("./lib/Task");
+const { curry } = require('ramda');
+const logger = msg => x =>  console.log(`${msg}: ${x}`)
 
 
-const sum1 = x =>  x + 1
+const sum1 = x =>  x + 1;
+const multBy = curry((x, y) => x * y);
+const add = curry((x, y)=> x + y);
+const molo = x => Task.of(x + 7) 
 
-const future = x => new Task((res, rej)=> {
+
+const taskRes = x => new Task((res, rej)=> {
     setTimeout(()=> {
         res(x)
     }, 100)
 });
-future(1).map(sum1).fork(console.log, console.error) // Hello
+
+
+const taskRej = err =>  new Task((_, rej) => setTimeout(()=> rej(err), 90));
+// // task(1).map(add(980)).map(sum1).chain(molo)
+// taskRes(1)
+// .map(add(2))
+// .chain(molo)
+// .fork(logger('[OK]'), logger('[KO]')) // Hello
+
+const tt = Task.of(x => x + 1);
+const t1 = Task.of(3);
+taskRes(2).chain(molo).ap(tt).fork(logger('[OK 1 ]'), logger('[KO 1]')) // Hello
