3bd0d21e0ceb827820f0374a078681bc6a32e182
Alvaro <alvaro.touzon@bbva.com>
2025-02-12 09:58:36 +0100

    feat: add query runner

--- index.mjs
+++ index.mjs
@@ -1,18 +1,25 @@
+import { curry } from "ramda";
 import Sum from "./lib/sum.js";
 import Task from "./lib/Task.js";
 import { liftA2 } from "./utils/index.js";
-// // const taskRes = curry((time, x) => new Task((res, rej)=> {
-// //     setTimeout(()=> {
-// //         res(x)
-// //     }, time)
-// // }));
+import { insideOut, logger } from "./utils/index.js";
 
-// // const taskRej = err => new Task((_, rej) => setTimeout(()=> rej(err), 90));
+const taskRes = curry((time, x) => new Task((_, resolve)=> {
+  setTimeout(()=> {
+    resolve(x)
+  }, time)
+}));
 
-const two = Sum.of(1).chain(x => Sum.of(x +1)).ap(Sum.of(x => x + 1));
-const one = liftA2(x => y => x + y, Sum.of(5), Sum.of(2));
-const three = liftA2(x => y => x + y, Task.of(5), Task.of(2));
-three.fork(x => console.log('[KO] ', x), x => console.log('[OK] ', x));
-console.log({two, one})
+
+const query = x => new Task((reject, resolve)=> {
+  fetch(`https://jsonplaceholder.typicode.com/todos/${x}`).then(response => response.text()).then(resolve).catch(reject);
+});
+
+const proc = x => query(x).chain(x => Task.of(JSON.parse(x)))
+
+const ids = [1,2,3];
+const seven = insideOut(Task, ids.map(proc));
+seven.fork(logger('KO'), logger('SEVEN'));
+
 
 
--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,12 @@
 import { curry } from "ramda";
 // lift2 :: Applicative f => (a -> b -> c, f a, f b) -> f c
 export const liftA2 = curry((f, a, b) => b.ap(a.map(f)))
+// append :: a -> [a] -> [a]
+const append = y => xs => xs.concat([y]);
 
+
+export const insideOut = (T, xs) => xs.reduce(
+  (acc, x) => liftA2(append, x, acc),
+  T.of([])) // To start us off!
+// logger :: String -> a -> String
+export const logger = curry((msg, x)=> console.log(`[${msg}]: `,x));
